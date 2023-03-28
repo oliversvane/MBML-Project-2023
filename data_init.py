ba1dfd991eff3d7b042f3e84f3b17f875050f584
oliversvane <oliversvaneolsen@gmail.com>
2023-03-28 20:40:52 +0200

    created plots and data ingest

--- data_init.py
+++ data_init.py
@@ -1,17 +1,2 @@
-import kaggle
-import os
 
-dir_x = "data/"
 
-kaggle.api.authenticate()
-kaggle.api.dataset_download_files(
-    "robikscube/flight-delay-dataset-20182022",
-    path=dir_x,
-    unzip=True,
-)
-for filename in os.listdir(dir_x):
-    f = os.path.join(dir_x, filename)
-    if f.m.endswith(".parquet") or filename == "Airlines.csv":
-        pass
-    else:
-        os.remove(f)
--- src/data/extract.py
+++ src/data/extract.py
@@ -1,10 +1,27 @@
 from pathlib import Path
 import pandas as pd
+from tqdm import tqdm
 
 
-def combine_parquet(data_path: str = "data/"):
+def combine_parquet(data_path: str = "data/") -> pd.DataFrame:
+    """
+    Reads all parquet files from a single directory, and returns a combined dataframe.
+
+    Parameters:
+    -----------------
+    data_path: str [Optional]
+        directory of data folder containing parquet files
+    
+        
+    Returns:
+    -----------------
+    full_df: pd.DataFrame
+        A combined dataframe of all the parquet files
+    
+    """
     data_dir = Path(data_path)
     full_df = pd.concat(
         pd.read_parquet(parquet_file) for parquet_file in data_dir.glob("*.parquet")
     )
     return full_df
+
